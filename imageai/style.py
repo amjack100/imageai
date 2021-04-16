8ec64d4d5c7be0c8c1a1170b544d7325d730b7e3
Andy Jackson <amjack100@gmail.com>
2021-04-16 15:17:33 -0400

    style module fix for pngs

--- imageai/style.py
+++ imageai/style.py
@@ -13,32 +13,72 @@ import numpy as np
 import time
 from PIL import Image
 
-""" The style transfer test with Arbitrary Image Stylization. """
-
-
-# export CUDA_VISIBLE_DEVICES=-1
-# os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
-# Print information about TensorFlow and make sure it's running with GPU support.
-
 
 class Unit:
+    """
+    The image stylization module
+    """
+
     def __init__(self, content_image, style_image, output_image) -> None:
 
         self.analysis_mode = False
-        self.content_image = plt.imread(content_image)
-        self.style_image = plt.imread(style_image)
+        self.images = (plt.imread(content_image), plt.imread(style_image))
         self.hub_module = (
             "https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2"
         )
         self.output_image = output_image
 
+    def _make_img_adjustments(self):
+        """
+        Make preprocessing adjustments based on image input file type and number
+        of channels
+        """
+
+        self.images = [img.astype(np.float32)[np.newaxis, ...] for img in self.images]
+        self.images = [img / 255.0 if np.max(img) > 1 else img for img in self.images]
+        self.images = [
+            img[:, :, :, 0:3] if img.shape[-1] == 4 else img for img in self.images
+        ]
+
+    def _save_normal(self, output):
+        """
+        Save the output file as expected with no extra annotations
+        """
+        img = Image.fromarray(tf.squeeze(output * 255, axis=0).numpy().astype(np.uint8))
+        img.save(self.output_image)
+
+    def _save_analytic(self, content, style, output):
+        """
+        Save a file showing the content, style, and output side by side
+        Not yet implemented into the cli
+        """
+
+        mpl.rcParams["figure.figsize"] = (12, 12)
+        mpl.rcParams["axes.grid"] = False
+
+        plt.subplot(1, 3, 1)
+        plt.imshow(tf.squeeze(content, axis=0))
+        plt.title("Content Image")
+
+        plt.subplot(1, 3, 2)
+        plt.imshow(tf.squeeze(style, axis=0))
+        plt.title("Style Image")
+
+        plt.subplot(1, 3, 3)
+        plt.imshow(tf.squeeze(output, axis=0))
+        plt.title("Output Image")
+
+        plt.savefig(self.output_image)
+
     def run(self):
-
+        """
+        Main calling method of the class which executes pre-processing,
+        processing, and saving steps sequentially
+        """
         time_start = time.time()
+        self._make_img_adjustments()
 
-        # Convert to float32 numpy array, add batch dimension, and normalize to range [0, 1]. Example using numpy:
-        content_image = self.content_image.astype(np.float32)[np.newaxis, ...] / 255.0
-        style_image = self.style_image.astype(np.float32)[np.newaxis, ...] / 255.0
+        (content_image, style_image) = self.images
 
         style_image = tf.image.resize(style_image, (256, 256))
 
@@ -50,28 +90,7 @@ class Unit:
 
         print("Elapsed time:", time_end - time_start, "sec")
 
-        # Adjust figure size for the plot.
-        mpl.rcParams["figure.figsize"] = (12, 12)
-        mpl.rcParams["axes.grid"] = False
-
-        # Show results.
-
         if self.analysis_mode:
-            plt.subplot(1, 3, 1)
-            plt.imshow(tf.squeeze(content_image, axis=0))
-            plt.title("Content Image")
-
-            plt.subplot(1, 3, 2)
-            plt.imshow(tf.squeeze(style_image, axis=0))
-            plt.title("Style Image")
-
-            plt.subplot(1, 3, 3)
-            plt.imshow(tf.squeeze(output_image, axis=0))
-            plt.title("Output Image")
-
-            plt.savefig(self.output_image)
+            self._save_analytic(content_image, style_image, output_image)
         else:
-            img = Image.fromarray(
-                tf.squeeze(output_image * 255, axis=0).numpy().astype(np.uint8)
-            )
-            img.save(self.output_image)
+            self._save_normal(output_image)
